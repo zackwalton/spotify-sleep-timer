6fa1fe91a17d404f3efd745bb735991d7391fdbc
Zack Walton <zackwalton15@gmail.com>
2022-11-13 02:10:39 -0500

    Added default values, user input, and comments

--- spotify_sleep.py
+++ spotify_sleep.py
@@ -1,13 +1,26 @@
 import os
 import time
 
-wait_time = 60*10
-program = "spotify.exe"
-print(f'Waiting: {wait_time} seconds before closing {program}')
+PROCESS_NAME = "spotify.exe"
+DEFAULT_LENGTH = 10  # default length of sleep timer in minutes
+COUNTDOWN_INTERVAL = 1 # length of a single countdown interval in seconds
 
-for second in range(wait_time, 0, -1):
-    os.system('cls')
-    print(f'{second // 60:02d}:{second % 60:02d} until \'{program}\' will be terminated.')
-    time.sleep(1)
+print("Just hit enter for default 10 minutes ->")
 
-os.system(f'taskkill/im {program}')
+while True:
+    try:
+        minutes = input("Sleep timer duration (minutes) : ") 
+        minutes = minutes if minutes else DEFAULT_LENGTH
+        
+        total_seconds = 60*int(minutes)  # user input for sleep timer length
+        break  
+    except ValueError:
+        print('\nERROR: Please enter a valid integer value for the duration in minutes.')
+
+for second in range(total_seconds, 0, -COUNTDOWN_INTERVAL):
+    print(f'{second // 60:02d}:{second % 60:02d} until \'{PROCESS_NAME}\' will be terminated.')  # print the remaining time
+    time.sleep(COUNTDOWN_INTERVAL)  # wait for countdown interval
+    os.system('cls')  # clear console
+
+# NOTE: not using the force option (/f) here allows spotify to keep the user's timestamp in the song
+os.system(f'taskkill/im {PROCESS_NAME}')  # kill program and all child processes 
